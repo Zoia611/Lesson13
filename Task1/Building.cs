92e479d17c2fd84c1263efff4718b4b1c4b578d8
Zoia611 <117438266+Zoia611@users.noreply.github.com>
2022-11-25 19:15:35 +0300

    Добавьте файлы проекта.

---
+++ Task1/Building.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Building
+    {
+        string adress;
+        int lenght;
+        int width;
+        int height;
+
+        string Adress
+        {
+            get
+            {
+                return adress;
+            }
+            set
+            {
+                if (value == "")
+                    adress = "Нет адреса";
+                else
+                    adress = value;
+            }
+        }
+        int Lenght
+        {
+            get
+            {
+                return lenght;
+            }
+            set
+            {
+                if (value < 1)
+                    lenght = 1;
+                else
+                    lenght = value;
+            }
+        }
+        int Width
+        {
+            get
+            {
+                return width;
+            }
+            set
+            {
+                width = value;
+            }
+        }
+        int Height
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                height = value;
+            }
+        }
+        public Building(string adress, int lenght, int width, int height)
+        {
+            Adress = adress;
+            Lenght = lenght;
+            Width = width;
+            Height = height;
+        }
+        public string Print()
+        {
+            return $"Адрес: {adress}; Длина здания: {lenght}; Ширина здания: {width}; Высота здания: {height}";
+        }
+    }
+        
+}
---
+++ Task1/MultiBuilding.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    sealed class MultiBuilding : Building
+    {
+
+        int floors;
+        int Floors
+        {
+            get
+            {
+                return floors;
+            }
+            set
+            {
+                if (value <= 0)
+                    floors = 1;
+                else
+                    floors = value;
+            }
+
+        }
+        public MultiBuilding(string adress, int lenght, int width, int height, int floors)
+            : base(adress, lenght, width, height)
+        {
+            Floors = floors;
+        }
+
+        public string Print()
+        {
+            string result = base.Print();
+            result += $"; Количество этажей: {floors}";
+            return result;
+        }
+    }
+        
+    }
